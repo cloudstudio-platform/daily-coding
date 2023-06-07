2fd93f419d9b0ce6639ba5af0226ed93491fd224
wangxiaoxiao <wangxiaoxiao@coding.net>
2023-06-07 18:13:09 +0800

    fix: #0 修改样式问题

--- daily-blog/components/CommuityLink.tsx
+++ daily-blog/components/CommuityLink.tsx
@@ -1,9 +1,10 @@
+// import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const CommunityLink = () => (
   <div className="mt-20 flex w-full justify-between mb:mt-10 mb:flex-col">
-    <div className="commuity-link-box rounded-lg">
+    <div className="commuity-link-box w-full rounded-lg">
       <p className="pb-4 text-[18px] font-medium leading-[25px]">腾讯云开发者社区</p>
       <p className="pb-12 text-[16px] leading-[22px] mb:pb-6">
         腾讯云开发者社区是腾讯云官方开发者社区，致力于打造开发者的技术分享型社区。
@@ -15,7 +16,7 @@ const CommunityLink = () => (
         立即查看 &rarr;
       </Link>
     </div>
-    <div className="commuity-link-box ml-10 rounded-lg mb:ml-0 mb:mt-6">
+    {/* <div className="commuity-link-box ml-10 rounded-lg mb:ml-0 mb:mt-6">
       <p className="pb-4 text-[18px] font-medium leading-[25px]">CSDN 开发者专栏</p>
       <p className="pb-12 text-[16px] leading-[22px] mb:pb-6">
         致力于为中国软件开发者提供知识传播、在线学习、职业发展等全生命周期服务。
@@ -23,7 +24,7 @@ const CommunityLink = () => (
       <Link href="/" className="text-[#0066ff] hover:opacity-80">
         立即查看 &rarr;
       </Link>
-    </div>
+    </div> */}
   </div>
 )
 export default CommunityLink
