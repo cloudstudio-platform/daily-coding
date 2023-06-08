e0b30f92222ef126e5693bdecf7383e49e48bdac
wangxiaoxiao <wangxiaoxiao@coding.net>
2023-06-08 14:12:48 +0800

    fix: 优化样式和代码

--- daily-blog/components/CommuityLink.tsx
+++ daily-blog/components/CommuityLink.tsx
@@ -1,20 +1,30 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const CommunityLink = () => (
   <div className="mt-20 flex w-full justify-between mb:mt-10 mb:flex-col">
-    <div className="commuity-link-box w-full rounded-lg">
-      <p className="pb-4 text-[18px] font-medium leading-[25px]">腾讯云开发者社区</p>
-      <p className="pb-12 text-[16px] leading-[22px] mb:pb-6">
-        腾讯云开发者社区是腾讯云官方开发者社区，致力于打造开发者的技术分享型社区。
-      </p>
-      <Link
-        href="https://cloud.tencent.com/developer/zone/cloudstudio?from=19165"
-        className="text-[#0066ff] hover:opacity-80"
-      >
-        立即查看 &rarr;
-      </Link>
+    <div className="commuity-link-box flex w-full rounded-lg">
+      <div className="mr-[72px] flex h-[175px] w-[260px] items-center justify-center rounded-[20px] bg-[#F5F7FA]">
+        <Image
+          src="https://help-assets.codehub.cn/enterprise/new-static/images/cs/tencent-logo.png"
+          width={147}
+          height={34}
+          alt={''}
+        />
+      </div>
+      <div className="pt-4">
+        <p className="pb-4 text-[18px] font-medium leading-[25px]">腾讯云开发者社区</p>
+        <p className="pb-12 text-[16px] leading-[22px] mb:pb-6">
+          腾讯云开发者社区是腾讯云官方开发者社区，致力于打造开发者的技术分享型社区。
+        </p>
+        <Link
+          href="https://cloud.tencent.com/developer/zone/cloudstudio?from=19165"
+          className="text-[#0066ff] hover:opacity-80"
+        >
+          立即查看 &rarr;
+        </Link>
+      </div>
     </div>
     {/* <div className="commuity-link-box ml-10 rounded-lg mb:ml-0 mb:mt-6">
       <p className="pb-4 text-[18px] font-medium leading-[25px]">CSDN 开发者专栏</p>
--- daily-blog/pages/index.tsx
+++ daily-blog/pages/index.tsx
@@ -1,14 +1,8 @@
-import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
-import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
-import { formatDate } from 'pliny/utils/formatDate'
 import { sortedBlogPost, allCoreContent } from 'pliny/utils/contentlayer'
-import { InferGetStaticPropsType } from 'next'
 import { allBlogs } from 'contentlayer/generated'
 import type { Blog } from 'contentlayer/generated'
-
-const MAX_DISPLAY = 5
 
 export const getStaticProps = async () => {
   const sortedPosts = sortedBlogPost(allBlogs) as Blog[]
@@ -17,69 +11,10 @@ export const getStaticProps = async () => {
   return { props: { posts } }
 }
 
-export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      {/* <div className="divide-y divide-gray-200">
-        <ul className="divide-y divide-gray-200">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link href={`/blog/${slug}`} className="text-[#171D26]">
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500">{summary}</div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600"
-                          aria-label={`Read "${title}"`}
-                        >
-                          阅读更多
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="mb-[104px] flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600"
-            aria-label="All posts"
-          >
-            博客列表 &rarr;
-          </Link>
-        </div>
-      )} */}
     </>
   )
 }
